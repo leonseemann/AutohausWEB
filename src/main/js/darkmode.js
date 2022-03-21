d1573d576733d6f89b9e1f21cc408b0649c6ef0a
Leon Seemann <le.see@web.de>
2022-03-21 19:59:03 +0100

    Copied test to main

--- src/main/js/darkmode.js
+++ src/main/js/darkmode.js
@@ -1,30 +1,58 @@
-var lightIcon = "fa-regular";
-var darkIcon = "fa-solid";
+var lightIcon = "fa-moon";
+var darkIcon = "fa-sun";
 
-if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-  document.documentElement.classList.add("dark");
-  document.getElementById("darkmode").classList.add(darkIcon);
-  console.log("Darkmode[Automatic]: Enabled");
-} else {
-  document.documentElement.classList.remove("dark");
-  document.getElementById("darkmode").classList.add(lightIcon);
-  console.log("Darkmode[Automatic]: Disabled");
+function tailwindToggle(){
+  if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    document.documentElement.classList.add("dark");
+    document.getElementById("darkmode").classList.add(darkIcon);
+    console.log("Darkmode[tailwindToggle]: Enabled");
+  } else {
+    document.documentElement.classList.remove("dark");
+    document.getElementById("darkmode").classList.add(lightIcon);
+    console.log("Darkmode[tailwindToggle]: Disabled");
+  }
 }
 
+function darkmodeEnabled() {
+  document.documentElement.classList.add("dark");
+  document.getElementById("darkmode").classList.remove(lightIcon);
+  document.getElementById("darkmode").classList.add(darkIcon);
+}
+
+function darkmodeDisabled() {
+  document.documentElement.classList.remove("dark");
+  document.getElementById("darkmode").classList.remove(darkIcon);
+  document.getElementById("darkmode").classList.add(lightIcon);
+}
+
+if (localStorage.getItem('isDarkmode') === null) {
+  tailwindToggle();
+} else {
+  if (localStorage.getItem('isDarkmode') === "true"){
+    darkmodeEnabled();
+    console.log("Darkmode[local]: Enabled");
+  } 
+    
+  if (localStorage.getItem('isDarkmode') === "false") {
+    darkmodeDisabled();
+    console.log("Darkmode[local]: Disabled");
+  }
+}
+
+
+
+
+
 function darkmodeToggle() {
-  var element = document.documentElement.classList;
-  var darkmode = document.getElementById("darkmode");
-  element.toggle("dark");
+  document.documentElement.classList.toggle("dark");
 
-  if (element.contains("dark")) {
-    darkmode.classList.remove(lightIcon);
-    darkmode.classList.add(darkIcon);
-
+  if (document.documentElement.classList.contains("dark")) {
+    darkmodeEnabled();
+    localStorage.setItem('isDarkmode', true);
     console.log("Darkmode[Button]: Enabled");
   } else {
-    darkmode.classList.remove(darkIcon);
-    darkmode.classList.add(lightIcon);
-
+    darkmodeDisabled();
+    localStorage.setItem('isDarkmode', false);
     console.log("Darkmode[Button]: Disabled");
   }
 }

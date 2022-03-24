1c033c8363c9998b59e4c1c7538898e12f998b74
Leon Seemann <le.see@web.de>
2022-03-24 12:30:29 +0100

    Added gif in index
    Added dark/whitemode icon

--- src/test/js/darkmode.js
+++ src/test/js/darkmode.js
@@ -5,10 +5,12 @@ function tailwindToggle(){
   if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
     document.documentElement.classList.add("dark");
     document.getElementById("darkmode").classList.add(darkIcon);
+    document.getElementById("logo").src="../resources/logos/logo_large_darkmode.png"
     console.log("Darkmode[tailwindToggle]: Enabled");
   } else {
     document.documentElement.classList.remove("dark");
     document.getElementById("darkmode").classList.add(lightIcon);
+    document.getElementById("logo").src="../resources/logos/logo_large_whitemode.png"
     console.log("Darkmode[tailwindToggle]: Disabled");
   }
 }
@@ -17,12 +19,14 @@ function darkmodeEnabled() {
   document.documentElement.classList.add("dark");
   document.getElementById("darkmode").classList.remove(lightIcon);
   document.getElementById("darkmode").classList.add(darkIcon);
+  document.getElementById("logo").src="../resources/logos/logo_large_darkmode.png"
 }
 
 function darkmodeDisabled() {
   document.documentElement.classList.remove("dark");
   document.getElementById("darkmode").classList.remove(darkIcon);
   document.getElementById("darkmode").classList.add(lightIcon);
+  document.getElementById("logo").src="../resources/logos/logo_large_whitemode.png"
 }
 
 if (localStorage.getItem('isDarkmode') === null) {

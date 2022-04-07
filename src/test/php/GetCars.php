61fa24f67ad1a3dfbc64feb2c512acab68f114a8
Leon Seemann <le.see@web.de>
2022-04-07 17:13:50 +0200

    BMW automatic pulls

--- src/test/php/GetCars.php
+++ src/test/php/GetCars.php
@@ -1,16 +1,27 @@
 <?php
     function printCar($marke){
         include 'conn.php';
-        $sql = "SELECT ATID, typ, modell, baujahr, kommentar, felgenzoll, felgenmaterial, sitzheizung, lenkradheizung, schiebedach, farbe, farbematerial, innenraummaterial, sitzmaterial, verbrauch, getriebe, kraftstoff, hubraum, ps, preis FROM auto WHERE hersteller = {$marke} JOIN ausstattung ON auto.ASID = ausstattung.ASID JOIN motor ON auto.MTID = motor.MTID ORDER BY auto.hersteller, auto.typ ASC";
+        //$sql = "SELECT  modell, baujahr, kommentar, preis FROM auto LEFT JOIN ausstattung ON 'ausstattung.ASID' = 'auto.ASID' WHERE auto.hersteller = '{$marke}'";
+        $sql = "SELECT typ, modell, baujahr, hersteller, kommentar, bild, bildendung, felgenzoll, felgenmaterial, sitzheizung, lenkradheizung, schiebedach, farbe, farbematerial, innenraummaterial, sitzmaterial, verbrauch, getriebe, kraftstoff, hubraum, ps, preis 
+                FROM auto 
+                LEFT JOIN ausstattung ON auto.ASID = ausstattung.ASID 
+                LEFT JOIN motor ON auto.MTID = motor.MTID 
+                WHERE auto.hersteller = '{$marke}' 
+                ORDER BY auto.hersteller, auto.typ ASC";
         $abfrage = mysqli_query($conn, $sql);
         while ($ausgabe = mysqli_fetch_assoc($abfrage)) {
 ?>
             <div class="w-44">
                 <div>
-                    <p class="text-center header-sections"><?php $ausgabe['modell'] ?></p>
+                    <p class="text-center header-sections"><?php echo $ausgabe['modell'] ?></p>
                 </div>
                 <div>
-                    <img src="./resources/none.svg" class="ausstattung-img" alt="audi">
+                    <img src="data:image/<?php echo $ausgabe['bildendung']?>;charset=utf8;base64,<?php echo base64_encode($ausgabe['bild']); ?>"  class="ausstattung-img" alt="audi">
+                </div>
+                <div class="text-white">
+                    <p class="bg-red-400 break-words">
+                    <?php echo $ausgabe['kommentar'] ?>
+                    </p>
                 </div>
                 <div>
                     <ul>
@@ -31,11 +42,6 @@
                         <li class="ausstattung"><?php echo $ausgabe['ps']; ?></li>
                         <li class="ausstattung"><?php echo $ausgabe['preis']; ?></li>
                     </ul>
-                </div>
-                <div>
-                    <p>
-                        <?php echo $ausgabe['kommentar']; ?>
-                    </p>
                 </div>
             </div>
 <?php
